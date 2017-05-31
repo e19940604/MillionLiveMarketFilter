902d358a414911f5919145872789876946cbe4ff
xgnid <e19940604@gmail.com>
2017-05-31 13:20:43 +0800

    fix home

--- routes/web.php
+++ routes/web.php
@@ -10,5 +10,7 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/', 'HomeController@index');
+Route::get('/', function () {
+    return view('welcome');
+});
 Route::post('marketList', 'MarketController@listCreate');
