23fa47b66eb38612155b8fd7371d879260755c33
xgnid <e19940604@gmail.com>
2017-08-30 01:17:48 +0800

    Revert "fix skill range search"
    This reverts commit 19488c086c99fa62df694bd5a36b1762536c1093.

--- resources/views/million/bazaar.blade.php
+++ resources/views/million/bazaar.blade.php
@@ -117,15 +117,14 @@
                         <!-- line -->
                         <div class="row">
                             <div class="form-group">
-                                <label for="Card" class="col-sm-2 control-label">技能範圍</label>
+                                <label for="Card" class="col-sm-2 control-label">世界線</label>
                                 <div class="col-sm-8">
-                                    <select class="form-control" name="range">
-                                        <option value="0" >--無指定--</option>
-                                        <option value="">無技能</option>
-                                        <option value="AP">AP</option>
-                                        <option value="DP">DP</option>
-                                        <option value="AP/DP">AP/DP</option>
-
+                                    <select class="form-control" name="line">
+                                        <option value="0" >--全線--</option>
+                                        <option value="1">1</option>
+                                        <option value="2">2</option>
+                                        <option value="3">3</option>
+                                        <option value="4">4</option>
                                     </select>
                                 </div>
                             </div>
