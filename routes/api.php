4d7ea365b32cc26953b7a798e4a47be69e8e9b86
xgnid <e19940604@gmail.com>
2017-09-02 00:08:37 +0800

    hide face

--- routes/api.php
+++ routes/api.php
@@ -14,7 +14,7 @@ use Illuminate\Http\Request;
 */
 Route::group( []  ,function(){
     Route::post('marketList', 'MarketController@listCreate');
-    Route::get('marketList/{firstId?}/{name?}/{type?}/{idol?}/{cost?}/{skillPower?}/{line?}/{candyOrDrink?}/{page?}', 'MarketController@firstNewList');
+    //Route::get('marketList/{firstId?}/{name?}/{type?}/{idol?}/{cost?}/{skillPower?}/{line?}/{candyOrDrink?}/{page?}', 'MarketController@firstNewList');
 
 });
 
--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,7 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('test', 'MarketController@test');
-Route::get('/{page?}', 'MarketController@showIndex');
+//Route::get('test', 'MarketController@test');
+//Route::get('/{page?}', 'MarketController@showIndex');
 
 //Route::post('marketList', 'MarketController@listCreate');
